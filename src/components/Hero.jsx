a4f3992ddc39a8856cda8923a5783a7a669b6ec3
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-26 14:49:11 -0500

    created the hero section and did the main wireframe layout

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="hero-content">
+      <div className="hero">
+      
+      </div>
+      <div className="blocks">
+        <div id='block1'></div>
+        <div id='block2'></div>
+        <div id='block3'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
