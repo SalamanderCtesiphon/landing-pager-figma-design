d1599143771b70ad0a0b440626028aca9fcd2960
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-26 16:17:29 -0500

    implemented the hero section from the figma design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import navL from '../icons/navL.svg'
+import navR from '../icons/navR.svg'
 
 function Hero() {
   return (
     <div className="hero-content">
       <div className="hero">
+        <div className="hero-container1">
+          <img id='navL' src={navL} alt="navigate left" />
+        </div>
+        <div className="hero-container2">
+          <small>Univy University</small>
+          <h1 id="banner-title">Discover Your <br></br>Future at <span id="banner">Univy</span></h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae veniam qui rerum.</p>
+          <button id='apply-btn'>Apply Now <img id='navicon' src={navR} alt="navicon" /></button>
+        </div>
+        <div className="hero-container3">
+          <img id='navR' src={navL} alt="navigate left" />
+        </div>
       
       </div>
       <div className="blocks">
-        <div id='block1'></div>
-        <div id='block2'></div>
-        <div id='block3'></div>
+        <div id='block1'>
+          <h4>Education</h4>
+          <h2>Intensive Study</h2>
+        </div>
+        <div id='block2'>
+          <h4>Shop</h4>
+          <h2>Explore Debriefing Items</h2>
+        </div>
+        <div id='block3'>
+          <h4>Schedule</h4>
+          <h2>View Course Schedule</h2>
+        </div>
       </div>
     </div>
   )
