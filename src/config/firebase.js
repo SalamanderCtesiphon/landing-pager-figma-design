0c020bcdef5528d77b55bbdbef628780c9901f82
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-30 11:07:28 -0500

    deployed to firebase hosting

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC835ILi42U8utfLbV6HMVxSO1nuDyNe-E",
+  authDomain: "landing-page-1059a.firebaseapp.com",
+  projectId: "landing-page-1059a",
+  storageBucket: "landing-page-1059a.appspot.com",
+  messagingSenderId: "198765543008",
+  appId: "1:198765543008:web:3e8acee84beb30e8044d05"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
